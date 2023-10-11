40a362a37e3c7d98104315b10bff65d0351d9832
Mihai-VladTwitchTV <mitzu.v99444@yahoo.com>
2023-10-11 15:12:45 +0300

    Probl 2 si 3 fara teste inca Probl 2,3,4(incompl) fara teste inca

--- src/ElektroKauf.java
+++ src/ElektroKauf.java
@@ -3,8 +3,8 @@ public class ElektroKauf {
     public int[] usb;
     public int maxKeyboards;
     public int maxUSB;
-    public int keyboardAmmount;
-    public int usbAmmount;
+    public int keyboardAmmount = 0;
+    public int usbAmmount = 0;
     ElektroKauf(int maxUSBS,int maxKeyboards_){
         keyboards = new int[maxKeyboards_];
         usb = new int[maxUSBS];
@@ -12,13 +12,49 @@ public class ElektroKauf {
         usbAmmount = 0;
     }
     public void addKeyboards(int price){
-        if(keyboardAmmount)
-        keyboards[keyboardAmmount] = price;
-        keyboardAmmount++;
-    }
-    public void addUSB(int price){
-        usb[usbAmmount] = price;
-        usbAmmount++;
+        if(keyboardAmmount < maxKeyboards-1)
+        { keyboards[keyboardAmmount] = price;
+        keyboardAmmount++;}
+    else{
+        throw new RuntimeException("Not enough space left");
+        }
     }
 
+        public void addUSB(int price) {
+            if (usbAmmount < maxUSB - 1) {
+                usb[usbAmmount] = price;
+                usbAmmount++;
+            } else {
+                throw new RuntimeException("Not enough space left");
+            }
+        }
+
+    public int cheapestKeyboard(){
+        int min = 44444444;
+    for(int i = 0;i < keyboardAmmount-1;i++){
+        if(keyboards[i] < min)
+            min = keyboards[i];
+        }
+    return min;
+    }
+
+    public int expensiveItem(){
+        int max = -1;
+        for(int i = 0;i<usbAmmount;i++) {
+            if (max < usb[i])
+                max = usb[i];
+            if(max < keyboards[i])
+                max = keyboards[i];
+        }
+        return max;
+    }
+
+    public int  expensiveByBudget(int budget){
+        int max = -1;
+        for(int i=0;i < keyboardAmmount;i++){
+            if(max < keyboards[i] && keyboards[i] <= budget)
+                max = keyboards[i];
+        }
+        return max;
+    }
 }
